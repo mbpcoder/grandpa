5d9e40177df336f4ae79440f70e264e1714fa00f
Mahdi Bagheri <mb.programmer@yahoo.com>
2018-10-15 02:15:56 +0330

    added Storage class

---
+++ src/Storage.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Grandpa;
+
+class Storage
+{
+    public function exists()
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function put()
+    {
+
+    }
+
+    public function size()
+    {
+
+    }
+
+    public function lastModified()
+    {
+
+    }
+
+    public function copy()
+    {
+
+    }
+
+    public function move()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function files()
+    {
+
+    }
+
+    public function allFiles()
+    {
+
+    }
+
+    public function directories()
+    {
+
+    }
+
+    public function allDirectories()
+    {
+
+    }
+
+    public function makeDirectory()
+    {
+
+    }
+
+    public function deleteDirectory()
+    {
+
+    }
+}
